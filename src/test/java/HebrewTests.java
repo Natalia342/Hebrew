d56399f7c651ac721b0638c9eac1dd02d1e56968
Natalia342 <nataliatalova7@gmail.com>
2023-03-05 22:56:59 +0200

    tests

---
+++ src/test/java/HebrewTests.java
@@ -0,0 +1,29 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class HebrewTests {
+    WebDriver wd;
+    @BeforeMethod
+    public void preCondition(){
+        wd=new ChromeDriver();
+        wd.navigate().to("https://www.zebratix.com/?gclid=Cj0KCQiA-rj9BRCAARIsANB_4ACKtBcEamSd_tPOiMk1ya5uSECytwfjY2rAHNFRFXMhja-vFRYzPM0aAmIqEALw_wcB");
+    }
+    @Test
+    public void testFind(){
+        WebElement element = wd.findElement(By.xpath("//div[@class='pace  pace-inactive']"));
+     //   System.out.println(element);
+
+
+    }
+    @AfterMethod
+    public void postCondition(){
+        //    wd.quit();
+    }
+}
